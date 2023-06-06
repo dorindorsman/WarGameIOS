81b18196d73f6d5dbd0fb35adb27ca27a0a8498e
Dorin Dorsman <dorsmandorin@gmail.com>
2023-06-06 20:20:38 +0300

    Initial Commit

---
+++ WarGame/WarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameApp.swift
+//  WarGame
+//
+//  Created by Student18 on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
